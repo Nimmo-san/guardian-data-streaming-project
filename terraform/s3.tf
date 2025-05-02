4152d04beafa9b189e7b2f05988e9e7b00d11f53
Nimmo-san <nat25oloman@gmail.com>
2025-05-02 14:19:05 +0100

    fixed mistake on naming bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "ingestion_bucket" {
-    bucket_prefix = "${vars.ingestion_bucket_prefix}"
+    bucket_prefix = "${var.ingestion_bucket_prefix}"
     tags = {
       name = "ingestion_bucket"
     }
