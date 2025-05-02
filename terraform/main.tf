45b7731add4ca55dbf81bfc6ea4f043fbabd8946
Nimmo-san <nat25oloman@gmail.com>
2025-05-02 14:16:28 +0100

    added providers/backend

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+
+# GDSP - guardian data streaming project
+  backend "s3" {
+    bucket = "gdsp-backend-tfstate"
+    key = "tfstate/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+    region = "eu-west-2"
+    default_tags {
+      tags = {
+        project = "gdsp"
+        environment = "dev"
+      }
+    }
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
