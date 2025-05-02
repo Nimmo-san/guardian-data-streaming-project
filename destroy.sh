ba53f58cf61ecdf8bb1df6391651bf79ff0f4cc9
Nimmo-san <nat25oloman@gmail.com>
2025-05-02 13:22:26 +0100

    basic destroy tf infrastructure script setup

---
+++ destroy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+
+
+# Vars
+PROJECT_NAME="guardian-data-streaming-project"
+TERRAFORM_DIR="./terraform"
+
+
+echo "========================================="
+echo " Starting Infrastructure Cleanup Process "
+echo "========================================="
