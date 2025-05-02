ae4719bffe37ac56d35ad5a078d82ce592f9a139
Nimmo-san <nat25oloman@gmail.com>
2025-05-03 00:05:40 +0100

    added api key retrieval from secrets manager

--- src/guardian_api.py
+++ src/guardian_api.py
@@ -1,34 +1,42 @@
 from typing import Optional, List, Dict
-from dotenv import load_dotenv
+import boto3
 import logging
 import requests
-import os
-from pprint import pprint
 
-load_dotenv()
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 GUARDIAN_API_URL = "https://content.guardianapis.com/search"
 
 
-def fetch_guardian_articles(
-        search_term: str,
-        date_from: Optional[str] = None,
-        page_size: int = 10
-    ) -> List[Dict]:
+def get_api_key(secret_name="guardian_api_key"):
+    client = boto3.client("secretsmanager", region_name="eu-west-2")
+    try:
+        response = client.get_secret_value(SecretId=secret_name)
+        return response.get("SecretString")
+    except Exception as e:
+        logger.error(f"Failed to retrieve secret: {e}")
+        return
 
-    guardian_api = os.getenv("GUARDIAN_API_KEY")
-    if not guardian_api:
-        logger.error(f"Guardian api key empty: {guardian_api}")
+
+def fetch_guardian_articles(
+    guardian_api_key: str,
+    search_term: str,
+    date_from: Optional[str] = None,
+    page_size: int = 10,
+) -> List[Dict]:
+
+    if not guardian_api_key:
+        logger.error(f"Guardian api key empty: {guardian_api_key}")
 
     params = {
         "q": search_term,
-        "api-key": guardian_api,
+        "api-key": guardian_api_key,
         "page-size": page_size,
         "order-by": "newest",
-        "show-fields": "all"
+        "show-fields": "all",
     }
 
     if date_from:
@@ -38,27 +46,36 @@ def fetch_guardian_articles(
     response = requests.get(GUARDIAN_API_URL, params=params)
 
     if response.status_code != 200:
-        logger.error(f"Received response - status code: {response.status_code}")
+        logger.error(
+            f"Received response - status code: {response.status_code}"
+        )
 
     logger.info(f"Response status code - {response.status_code}")
     data = response.json()
     articles = data.get("response", {}).get("results", [])
-
 
     simplified_articles = []
     for article in articles:
         preview = article.get("fields", {}).get("bodyText", "")
         content_preview = preview[:1000] if preview else ""
 
-        simplified_articles.append({
-            "webPublicationDate": article.get("webPublicationDate"),
-            "webTitle": article.get("webTitle"),
-            "webUrl": article.get("webUrl"),
-            "content_preview": content_preview
-        })
+        logger.info("Formatting data...")
+        simplified_articles.append(
+            {
+                "webPublicationDate": article.get("webPublicationDate"),
+                "webTitle": article.get("webTitle"),
+                "webUrl": article.get("webUrl"),
+                "content_preview": content_preview,
+            }
+        )
 
     logger.info("Returning formatted data...")
-    pprint(simplified_articles)
     return simplified_articles
 
-fetch_guardian_articles(search_term="finance", date_from="2025-01-01")
+
+# For testing
+if __name__ == "__main__":
+    api_key = get_api_key()
+    fetch_guardian_articles(
+        api_key, search_term="finance", date_from="2025-01-01"
+    )
