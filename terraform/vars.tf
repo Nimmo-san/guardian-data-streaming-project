affae816cec863aacbc89fef8b4b1c70cf80f9c7
Nimmo-san <nat25oloman@gmail.com>
2025-05-04 00:38:45 +0100

    added basic variables'

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,4 +1,19 @@
-variable "ingestion_bucket_prefix" {
-  type = string
-  default = "gdsp-bucket"
+variable "aws_region" {
+  default = "eu-west-2"
+}
+
+variable "lambda_function_name" {
+  default = "guardian_stream_lambda"
+}
+
+variable "guardian_api_secret_name" {
+  default = "guardian_api_key"
+}
+
+variable "python_runtime" {
+  default = "python3.11"
+}
+
+variable "s3_bucket_prefix" {
+  default = "gdsp"
 }
