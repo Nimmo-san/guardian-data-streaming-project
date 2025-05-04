7261c495c975928cc0680e90e02920dcdac4cb21
Nimmo-san <nat25oloman@gmail.com>
2025-05-04 01:00:10 +0100

    added: checking if deploy script is run from root/checking dependencies

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,31 @@ PROJECT_NAME="guardian-data-streaming-project"
 TERRAFORM_DIR="./terraform"
 
 
+
 echo "============================================="
 echo " Starting Infrastructure Deployment Process "
 echo "============================================="
 
 
+if [ ! -f "./deploy.sh" ]; then
+  echo "Run this script from the project root." >&2
+  exit 1
+fi
+
 # Checking TF Dependency
 if ! [ -x "$(command -v terraform )" ]; then
-    echo "❌ Terraform not installed. Install it first." >&2
+    echo "Terraform not installed. Install it first." >&2
     exit 1
 fi
 echo -e "👉 Terraform available!\n"
 
+# Building Lambda layer
+echo -r "Installing Lambda layer dependencies into ./layer/python...\n"
+mkdir -p layer/python
+pip install --target layer/python requests
+
 # TF Initialising
-echo -e "👉 Initialising Terraform!\n"
+echo -e "Initialising Terraform!\n"
 cd $TERRAFORM_DIR
 terraform init
 
@@ -28,12 +39,12 @@ echo -e "Intialised Terraform! ✅ \n"
 
 
 # Planning TF Config for TESTING
-echo -e "👉 Planning Terraform Configuration..."
+echo -e "Planning Terraform Configuration..."
 terraform plan 
 
 # Applying TF Config
 terraform apply -auto-approve
 
 
-echo "Infrastructure Deployment Complete! ✅ "
+echo -e "Infrastructure Deployment Complete! ✅ "
 
