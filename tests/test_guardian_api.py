adc791c78600a33e4fd9a4653654ef26fb0c938c
Nimmo-san <nat25oloman@gmail.com>
2025-05-04 15:22:41 +0100

    reformatted for pep8 compliance'

--- tests/test_guardian_api.py
+++ tests/test_guardian_api.py
@@ -46,24 +46,37 @@ class TestGuardianAPI:
         logger.info.assert_any_call("Calling api...")
         logger.info.assert_any_call("Returning formatted data...")
 
-    
     def test_fetch_guardian_articles_api_key_empty(self):
         # None api key
         invalid_api_key = None
         mock_logger = MagicMock()
 
-        articles = fetch_guardian_articles(guardian_api_key=invalid_api_key, search_term="", date_from="", logger=mock_logger)
+        articles = fetch_guardian_articles(
+            guardian_api_key=invalid_api_key,
+            search_term="",
+            date_from="",
+            logger=mock_logger,
+        )
         # Validating return value of empty list
         assert articles == []
 
-        mock_logger.error.assert_any_call(f"Guardian api key empty: {invalid_api_key}")
+        mock_logger.error.assert_any_call(
+            f"Guardian api key empty: {invalid_api_key}"
+        )
 
         # Empty api key
         invalid_api_key = ""
-        articles = fetch_guardian_articles(guardian_api_key=invalid_api_key, search_term="", date_from="", logger=mock_logger)
+        articles = fetch_guardian_articles(
+            guardian_api_key=invalid_api_key,
+            search_term="",
+            date_from="",
+            logger=mock_logger,
+        )
         # Validating return value of empty list
         assert articles == []
-        mock_logger.error.assert_any_call(f"Guardian api key empty: {invalid_api_key}")
+        mock_logger.error.assert_any_call(
+            f"Guardian api key empty: {invalid_api_key}"
+        )
 
     @patch("guardian_api.requests.get")
     def test_fetch_guardian_articles_invalid_api_key(self, mock_requests_get):
@@ -73,12 +86,20 @@ class TestGuardianAPI:
         status_code = 403
         mock_logger = MagicMock()
 
-
         mock_requests_get.return_value.status_code = status_code
-        mock_requests_get.return_value.json.return_value = {"response": {"results": []}}
-        fetch_guardian_articles(guardian_api_key=invalid_api_key, search_term="machine learning", date_from="2025-01-01", logger=mock_logger)
+        mock_requests_get.return_value.json.return_value = {
+            "response": {"results": []}
+        }
+        fetch_guardian_articles(
+            guardian_api_key=invalid_api_key,
+            search_term="machine learning",
+            date_from="2025-01-01",
+            logger=mock_logger,
+        )
 
-        mock_logger.error.assert_called_with(f"Received response - status code: {403}")
+        mock_logger.error.assert_called_with(
+            f"Received response - status code: {403}"
+        )
 
     # @pytest.mark.skip
     @patch("guardian_api.boto3.client")
@@ -98,22 +119,25 @@ class TestGuardianAPI:
         # Retrieving secret key and making sure it matches
         secret = get_api_key(secret_name="guardian_api_key", logger=logger)
         assert secret == mock_secret_value
-        logger.info.assert_any_call("Calling secrets manager for API Key -> guardian_api_key...")
+        logger.info.assert_any_call(
+            "Calling secrets manager for API Key -> guardian_api_key..."
+        )
         logger.info.assert_any_call("Found API Key")
-    
+
     @patch("guardian_api.boto3.client")
     def test_get_api_key_invalid_secret_name(self, mock_boto_client):
         mock_logger = MagicMock()
 
-        # Mocking secrets client that raises an error 
+        # Mocking secrets client that raises an error
         mock_client = MagicMock()
         mock_boto_client.return_value = mock_client
-        mock_client.get_secret_value.side_effect = Exception("Secret not found")
+        mock_client.get_secret_value.side_effect = Exception(
+            "Secret not found"
+        )
 
         secret = get_api_key(secret_name="fake_name", logger=mock_logger)
 
         assert secret is None
-        mock_logger.error.assert_called_with("Failed to retrieve secret: Secret not found")
-
-
-
+        mock_logger.error.assert_called_with(
+            "Failed to retrieve secret: Secret not found"
+        )
