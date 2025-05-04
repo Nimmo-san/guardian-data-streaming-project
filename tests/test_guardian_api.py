f8b8b6d77ef9c07ee8ed904c90991546e0e4fd28
Nimmo-san <nat25oloman@gmail.com>
2025-05-04 15:11:23 +0100

    added more tests for invalid/empty api key

--- tests/test_guardian_api.py
+++ tests/test_guardian_api.py
@@ -46,6 +46,34 @@ class TestGuardianAPI:
         logger.info.assert_any_call("Calling api...")
         logger.info.assert_any_call("Returning formatted data...")
 
+    
+    def test_fetch_guardian_articles_api_key_empty(self):
+        invalid_api_key = None
+        mock_logger = MagicMock()
+
+        articles = fetch_guardian_articles(guardian_api_key=invalid_api_key, search_term="", date_from="", logger=mock_logger)
+        assert articles == []
+
+        mock_logger.error.assert_any_call(f"Guardian api key empty: {invalid_api_key}")
+
+        invalid_api_key = ""
+        articles = fetch_guardian_articles(guardian_api_key=invalid_api_key, search_term="", date_from="", logger=mock_logger)
+        assert articles == []
+        mock_logger.error.assert_any_call(f"Guardian api key empty: {invalid_api_key}")
+
+    @patch("guardian_api.requests.get")
+    def test_fetch_guardian_articles_invalid_api_key(self, mock_requests_get):
+        invalid_api_key = "invalid-api-key"
+        status_code = 403
+        mock_logger = MagicMock()
+
+
+        mock_requests_get.return_value.status_code = status_code
+        mock_requests_get.return_value.json.return_value = {"response": {"results": []}}
+        fetch_guardian_articles(guardian_api_key=invalid_api_key, search_term="machine learning", date_from="2025-01-01", logger=mock_logger)
+
+        mock_logger.error.assert_called_with(f"Received response - status code: {403}")
+
     # @pytest.mark.skip
     @patch("guardian_api.boto3.client")
     def test_get_api_key_success(self, mock_boto_client):
@@ -63,3 +91,8 @@ class TestGuardianAPI:
         # Retrieving secret key and making sure it matches
         secret = get_api_key(secret_name="guardian_api_key", logger=logger)
         assert secret == "mock-secret-value"
+    
+    def test_get_api_key_invalid_secret_name(self):
+        pass
+
+
