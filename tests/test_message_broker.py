e436899cc435e3d9cff746b064e54bbcc824cbf2
Nimmo-san <nat25oloman@gmail.com>
2025-05-05 14:38:59 +0100

    invalid sqs address test complete with recent changes

--- tests/test_message_broker.py
+++ tests/test_message_broker.py
@@ -1,10 +1,12 @@
 from unittest.mock import patch, MagicMock
 from message_broker import send_message_to_sqs
+from botocore.exceptions import ClientError
+import pytest
 
 
 class TestMessageBroker:
     @patch("message_broker.boto3.client")
-    def test_send_message_to_sqs(self, mock_boto_client):
+    def test_send_message_to_sqs_success(self, mock_boto_client):
         # Mock the logger
         logger = MagicMock()
 
@@ -34,3 +36,37 @@ class TestMessageBroker:
 
         # Checking logger was called
         logger.info.assert_called_with("Message sent to SQS with ID: abc-123")
+
+    @patch("message_broker.boto3.client")
+    def test_send_message_to_sqs_invalid_address(self, mock_boto_client):
+        mock_sqs = MagicMock()
+        mock_logger = MagicMock()
+        mock_boto_client.return_value = mock_sqs
+
+        messages = [{"webTitle": "Test", "webUrl": "https://example.com"}]
+        queue_url = "invalid"
+
+        # Simulate the exact AWS ClientError
+        mock_sqs.send_message.side_effect = ClientError(
+            error_response={
+                "Error": {
+                    "Code": "InvalidAddress",
+                    "Message": (
+                        "An error occurred "
+                        "(InvalidAddress) when calling the SendMessage "
+                        "operation: "
+                        "The address invalid is not valid for this endpoint."
+                    ),
+                }
+            },
+            operation_name="SendMessage",
+        )
+
+        #
+        with pytest.raises(ClientError):
+            send_message_to_sqs(
+                messages=messages, queue_url=queue_url, logger=mock_logger
+            )
+
+        # error = exc_info.value
+        # assert error.response["Error"]["Code"] == "InvalidAddress"
